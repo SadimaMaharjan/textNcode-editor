1924f3014b84e9a250f9a44bfbb18f3a294d460d
Sadima Maharjan <npsadimamaharjan@gmail.com>
2023-07-09 23:18:31 +1000

    added readme

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,10 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
-const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+const { InjectManifest } = require("workbox-webpack-plugin");
+
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
